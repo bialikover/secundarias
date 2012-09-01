172eaf51c056f1d13858b714956251b55b6579a0
dhararon <dhararon@hotmail.com>
2012-09-01 12:24:39 -0500

    CRUD

--- application/controllers/alumnos.php
+++ application/controllers/alumnos.php
@@ -24,20 +24,24 @@ class Alumnos extends CI_Controller{
 		*/
 
 		$crud = new grocery_CRUD();
-		//$crud->set_theme('datatables');
+		$crud->set_theme('datatables');
     	$crud->set_table('alumnos');
     	$crud->columns('matricula','nombre','apellido_pat','apellido_mat','genero');
     	$crud->display_as('apellido_pat','Apellido paterno');
-    	$crud->fields('matricula','nombre','apellido_pat','apellido_mat','genero','ver');
+    	$crud->fields('matricula','nombre','apellido_pat','apellido_mat','genero');
+    	$crud->add_action('Ver', '', 'alumnos/show','ui-icon-plus');
+    	$crud->unset_delete();
  
     	$output = $crud->render();
     	$this->load->view('alumnos/index.html',$output);
 
 	}
 
-	public function ver()
+	public function show()
 	{
-
+		$id = $this->uri->segment(3);
+		$data['alumno'] = $this->db->get_where( 'alumnos', array( 'id' => $id ) )->result();
+		$this->load->view('alumnos/show.html',$data);
 	}
 
 	public function eliminar()
