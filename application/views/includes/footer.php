87cd8eeffe6f0475fc2c8aec394484ffc84edc8f
Martin Martinez <martinez.mz10@gmail.com>
2012-09-01 11:29:25 -0500

    Se agrego la estructura para css, jquery y imagenes más framework de Bootstrap

---
+++ application/views/includes/footer.php
@@ -0,0 +1,8 @@
+
+	<footer>
+		<script type="text/javascript" src="../../../assets/js/jquery.js"></script>
+		<script type="text/javascript" src="../../../assets/js/bootstrap.js"></script>
+	</footer>
+
+	</body>
+</html>
---
+++ application/views/includes/header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    	<title>Mi secu</title>
+		<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>/assets/css/bootstrap.css">
+		<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>/assets/css/bootstrap-responsive.css">
+		<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>/assets/css/secundaria.css">
+	</head>
+	<body>
+	<header>
+		<div span="row-fluid">
+			<div class="span12">
+				<ul class="my-nav">
+					<a class="my-tab" href="">hey</a>
+
+				<ul>
+			</div>
+		</div>
+
+	</header>
