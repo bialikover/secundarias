db4d540f4a170e79c9b222d5baaa358c29c5cb07
mmmartin <martinez.mz10@gmail.com>
2012-09-02 18:48:11 -0500

    hay va de nuevo

--- application/views/includes/header.php
+++ application/views/includes/header.php
@@ -4,19 +4,37 @@
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
     	<title>Mi secu</title>
-
-		<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/css/secundaria.css">
-
+		
+		<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>/assets/css/secundaria.css">
+		
+		
+<style type='text/css'>
+body
+{
+	font-family: Arial;
+	font-size: 14px;
+}
+a {
+    color: blue;
+    text-decoration: none;
+    font-size: 14px;
+}
+a:hover
+{
+	text-decoration: underline;
+}
+</style>
+		
 		<script type="text/javascript" src="<?php echo base_url();?>assets/js/jquery.js"></script>
 	</head>
 	<body>
 	<header>
 				<div class=" my-nav">
-					<a  href="<?php echo base_url("index.php/evento/index")?>"> <div class="my-tab"><img src="<?php echo base_url();?>/assets/img/home-bco.png"><div>Inicio</div></div></a>
-					<a  href=""> <div class="my-tab"> <img src="<?php echo base_url();?>/assets/img/docente-bco.png"><div>Docentes</div></div></a>
-					<a  href=""> <div class="my-tab"><img src="<?php echo base_url();?>/assets/img/alumno-bco.png"><div>Alumnos</div></div></a>
-					<a  href=""> <div class="my-tab"><img src="<?php echo base_url();?>/assets/img/materia-bco.png"><div>Materias</div></div></a>
-					<a  href=""> <div class="my-tab"><img src="<?php echo base_url();?>/assets/img/grupo-bco.png"><div>Grupos</div></div></a>
+					<a  href="<?php echo base_url("index.php/home/index")?>"> <div class="my-tab"><img src="<?php echo base_url();?>/assets/img/home-bco.png"><div>Inicio</div></div></a>
+					<a  href="<?php echo base_url("index.php/docentes/index")?>"> <div class="my-tab"> <img src="<?php echo base_url();?>/assets/img/docente-bco.png"><div>Docentes</div></div></a>
+					<a  href="<?php echo base_url("index.php/Alumnos/index")?>"> <div class="my-tab"><img src="<?php echo base_url();?>/assets/img/alumno-bco.png"><div>Alumnos</div></div></a>
+					<a  href="<?php echo base_url("index.php/materias/index")?>"> <div class="my-tab"><img src="<?php echo base_url();?>/assets/img/materia-bco.png"><div>Materias</div></div></a>
+					<a  href="<?php echo base_url("index.php/grupos/index")?>"> <div class="my-tab"><img src="<?php echo base_url();?>/assets/img/grupo-bco.png"><div>Grupos</div></div></a>
 				</div>
 		
 	</header>
---
+++ application/views/welcome/index.php
@@ -0,0 +1,66 @@
+<div class="my-sub-heder">
+<h1>Martín Martínez López</h1>
+</div>
+<div class="my-container-materias">
+
+	<div class="my-materia">
+		<div class="my-materia-inner">
+			<h3>Matematicas 1</h3>
+		</div>
+	</div>
+
+	<div class="my-materia">
+		<div class="my-materia-inner">
+			<h3>Matematicas 1</h3>
+		</div>
+	</div>
+
+	<div class="my-materia">
+		<div class="my-materia-inner">
+			<h3>Matematicas 1</h3>
+		</div>
+	</div>
+
+	<div class="my-materia">
+		<div class="my-materia-inner">
+			<h3>Matematicas 1</h3>
+		</div>
+	</div>
+
+	<div class="my-materia">
+		<div class="my-materia-inner">
+			<h3>Matematicas 1</h3>
+		</div>
+	</div>
+
+	<div class="my-materia">
+		<div class="my-materia-inner">
+			<h3>Matematicas 1</h3>
+		</div>
+	</div>
+
+	<div class="my-materia">
+		<div class="my-materia-inner">
+			<h3>Matematicas 1</h3>
+		</div>
+	</div>
+
+	<div class="my-materia">
+		<div class="my-materia-inner">
+			<h3>Matematicas 1</h3>
+		</div>
+	</div>
+
+	<div class="my-materia">
+		<div class="my-materia-inner">
+			<h3>Matematicas 1</h3>
+		</div>
+	</div>
+
+	<div class="my-materia">
+		<div class="my-materia-inner">
+			<h3>Matematicas 1</h3>
+		</div>
+	</div>
+	
+</div>
