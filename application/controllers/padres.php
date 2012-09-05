da7424045825b5d3c055d64d05c3b2595469794c
Adánowsky Galván González <bialikoer@gmail.com>
2012-09-04 23:33:55 -0500

    merge devel

---
+++ application/controllers/padres.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Description of padres
+ *
+ * @author neto
+ */
+class Padres extends CI_Controller {
+    public function __construct()
+    {
+            parent::__construct();
+            // Your own constructor code
+            $this->load->database();
+			$this->load->library('grocery_CRUD');
+    }
+    //trae todas las materias
+    public function index()
+    {
+
+    	$crud = new grocery_CRUD();
+		$crud->set_theme('datatables');
+		$crud->set_subject('padre');
+    	$crud->set_table('padres');
+    	$crud->columns('nombre','telefono','direccion');
+    	$crud->fields('nombre','telefono','direccion');
+    	$crud->add_action('Ver', '', 'padres/show','ui-icon-plus');
+    	$crud->unset_delete();
+ 
+    	$output = $crud->render();
+        $this->load->view('includes/header');
+        $this->load->view('padres/index',$output);
+        $this->load->view('includes/footer');
+    	
+    	
+    }
+    //muestra una materia
+    public function show()
+    {
+    	$this->load->view("padres/show");	
+    }
+
+    //muestra un formulario para crear una nueva materia
+    public function new_view()
+    {
+    	$this->load->view("padres/new");
+    }
+}
+
+?>
---
+++ application/views/aula_digital/comentarios.php
@@ -0,0 +1,159 @@
+<div class="my-perfil-container">
+
+	<div id="titlecover">
+
+
+
+
+<!-- Aqui va el numero de comentarios<div class="__like right" data-app="forums" data-area="topics" data-relid="2" data-isfave="">
+	<span class="ipsButton_extra right " title="0 member(s) follow this topic" data-tooltip="0 member(s) follow this topic"><img src="http://ipbskinning.com/demoboard/public/style_images/beacon/icon_users.png"> <strong>0</strong></span>
+
+</div>-->
+
+	<img src="http://www.gravatar.com/avatar/8cf6a9812232d9c87901c0240aab9067?s=100&amp;d=http%3A%2F%2Fipbskinning.com%2Fdemoboard%2Fpublic%2Fstyle_images%2Fbeacon%2Fprofile%2Fdefault_large.png" class="ipsUserPhoto ipsUserPhoto_medium left">
+
+<div itemscope="" itemtype="http://schema.org/Article" class="ipsBox_withphoto">
+			
+	<h1 itemprop="name" class="ipsType_pagetitle titulo_topic">Another Test Topic</h1>
+	<div class="desc lighter blend_links">
+		 Started by <span itemprop="creator">
+	pezza
+</span>, <span itemprop="dateCreated">Jul 24 2012 06:57 PM</span>
+	</div>
+	
+	<meta itemprop="interactionCount" content="UserComments:4">
+</div>
+</div>
+
+<!-- Aqui va el comentario -->
+<div class="topic hfeed clear clearfix">
+	
+	
+
+	<div class="ipsBoxxx">
+		<div class="ipsBox_container" id="ips_Posts">
+			
+				<!--post:2-->
+
+<div class="post_block hentry clear clearfix " id="post_id_2">
+		<a id="entry2"></a>
+		
+	
+		
+		<div class="post_wrap">
+			
+				<h3 class="row2">
+			
+				
+				<span class="post_id right ipsType_small desc blend_links">
+					
+						<a itemprop="replyToUrl" href="http://ipbskinning.com/demoboard/index.php?/topic/2-another-test-topic/#entry2" rel="bookmark" title="Another Test Topic: post #1">
+					
+					#1
+					</a>
+				</span>
+				
+					<span itemprop="creator name" class="author vcard">
+	pezza
+</span>
+				
+			
+				
+			</h3>
+			<div class="author_info">
+				<div itemscope="" itemtype="http://schema.org/Person" class="user_details">
+	<span class="hide" itemprop="name">pezza</span>
+	<ul class="basic_info">
+		
+			<p class="desc member_title">Administrator</p>
+		
+		<li class="avatar">
+				
+				
+					<img itemprop="image" src="http://www.gravatar.com/avatar/8cf6a9812232d9c87901c0240aab9067?s=100&amp;d=http%3A%2F%2Fipbskinning.com%2Fdemoboard%2Fpublic%2Fstyle_images%2Fbeacon%2Fprofile%2Fdefault_large.png" class="ipsUserPhoto ipsUserPhoto_large">
+				
+				
+			</li>
+		<li class="group_title">
+			<span style="color:red;">Administrators</span>
+		</li>
+		<li class="group_icon">
+			
+				<img src="http://ipbskinning.com/demoboard/public/style_extra/team_icons/admin.png" alt="">
+			
+			</li>
+		
+		<li class="post_count desc lighter">
+			3 posts
+		</li>
+		
+		
+	</ul>
+	
+	
+		<ul class="custom_fields">
+			
+				
+					
+				
+			
+		</ul>
+	
+	
+</div>
+			</div>
+			<div class="post_body">
+				<p class="posted_info desc lighter ipsType_small">
+					Posted <abbr class="published" itemprop="commentTime" title="2012-07-24T18:57:20+00:00">24 July 2012 - 06:57 PM</abbr>
+				</p>
+				
+				<div itemprop="commentText" class="post entry-content ">
+					<!--cached-Sun, 02 Sep 2012 12:43:35 +0000-->Blah blah blah <img src="http://ipbskinning.com/demoboard/public/style_emoticons/default/smile.png" class="bbc_emoticon" alt=":)"><br>
+<br>
+And a smiley! <img src="http://ipbskinning.com/demoboard/public/style_emoticons/default/biggrin.png" class="bbc_emoticon" alt=":D"> <img src="http://ipbskinning.com/demoboard/public/style_emoticons/default/tongue.png" class="bbc_emoticon" alt=":P">
+					
+					<br>
+					
+				</div>
+				
+				
+				
+<script type="text/javascript">
+	ipb.global.registerReputation( 'rep_post_2', { domLikeStripId: 'like_post_2', app: 'forums', type: 'pid', typeid: '2' }, parseInt('0') );
+</script>
+				
+								
+				
+				
+				
+			
+				<ul id="postControlsNormal_2" class="post_controls clear clearfix">
+					<li class="top hide"><a href="#ipboard_body" class="top" title="Back to top">Back to top</a></li>
+					<li><a href="http://ipbskinning.com/demoboard/index.php?app=forums&amp;module=post&amp;section=post&amp;do=reply_post&amp;f=2&amp;t=2&amp;qpid=2" class="_ips_trigger_quote ipsButton_secondary" pid="2" title="Reply directly to this post">Quote</a></li>
+							
+								<li class="multiquote" id="multiq_2" style="">
+							
+								<a href="http://ipbskinning.com/demoboard/index.php?app=forums&amp;module=post&amp;section=post&amp;do=reply_post&amp;f=2&amp;t=2&amp;qpid=2" title="MultiQuote allows you to select multiple posts across multiple topics, then reply to them all at once" class="ipsButton_secondary">MultiQuote</a>
+							</li>
+					
+					
+					
+					
+					<li class="report">
+							<a href="http://ipbskinning.com/demoboard/index.php?app=core&amp;module=reports&amp;rcom=post&amp;tid=2&amp;pid=2&amp;st=">Report</a>
+						</li>
+						
+				</ul>
+					
+			</div>
+		</div>
+	</div>
+		
+	</div>
+</div>
+
+<!-- Close topic -->
+</div>
+
+</div><!-- fin de my perfil -->
+
