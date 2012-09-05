e0df545f5a98ad597b220ee985dc838e17ed0a68
localadmin <localadmi@server.telmex.com>
2012-09-05 06:36:00 -0500

    Mensajes vista index

--- application/controllers/mensajes.php
+++ application/controllers/mensajes.php
@@ -2,14 +2,20 @@
 
 class Mensajes extends CI_Controller {
 
-    function __construct() {
-        parent::__construct();
-        $this->load->database();
-        $this->load->helper('url');
-        $this->load->library('grocery_CRUD');
-    }
+        function __construct() {
+            parent::__construct();
+            $this->load->database();
+            $this->load->helper('url');
+            $this->load->library('grocery_CRUD');
+        }
+        
+        public function index() {
+            //if($this->session->userdata('role') != 'Escuela')
+            $data = array();
+            $this->load->view('includes/header');
+            $this->load->view('mensajes/index', $data);
+            $this->load->view('includes/footer');
+        }
 
-    public function index() {
     }
-    
 ?>
---
+++ application/views/mensajes/index.php
@@ -0,0 +1,71 @@
+<div class="container-fluid">
+	<div class="row-fluid"> 
+		<div id="my-menu" class="span1">
+
+		</div>
+		<div id="my-menu" class="span10">
+
+			<div class="my-comentary-container">
+			<div class="my-comentary-header">Comentarios </div>
+			<div class="my-comentary">
+			<div class="row-fluid">
+				<div class="span2 my-center">
+					<img class="my-foto-mini" src="<?php echo base_url();?>/assets/img/user1.png">
+				</div>
+				<div class="span10">
+					<h4>Dharwin Borracho</h4>
+					<p>Esta es una publicación del Dharwin. 
+						Esta es una publicación del Dharwin. 
+						Esta es una publicación del Dharwin. 
+						Esta es una publicación del Dharwin. </p>
+
+						<div class="date-footer">
+						<span>05/09/2012 12:00:00</span>
+					</div>
+				</div>
+			</div>
+			</div>
+			<div class="my-comentary">
+			<div class="row-fluid">
+				<div class="span2 my-center">
+					<img class="my-foto-mini" src="<?php echo base_url();?>/assets/img/user1.png">
+				</div>
+				<div class="span10">
+					<h4>Dharwin Borracho</h4>
+					<p>Esta es una publicación del Dharwin. 
+						Esta es una publicación del Dharwin. 
+						Esta es una publicación del Dharwin. 
+						Esta es una publicación del Dharwin. </p>
+
+						<div class="date-footer">
+						<span>05/09/2012 12:00:00</span>
+					</div>
+				</div>
+			</div>
+			</div>
+			<div class="my-comentary">
+			<div class="row-fluid">
+				<div class="span2 my-center">
+					<img class="my-foto-mini" src="<?php echo base_url();?>/assets/img/user1.png">
+				</div>
+				<div class="span10">
+					<h4>Dharwin Borracho</h4>
+					<p>Esta es una publicación del Dharwin. 
+						Esta es una publicación del Dharwin. 
+						Esta es una publicación del Dharwin. 
+						Esta es una publicación del Dharwin. </p>
+
+						<div class="date-footer">
+						<span>05/09/2012 12:00:00</span>
+					</div>
+				</div>
+			</div>
+			</div>
+		</div>
+		</div>
+		<div id="my-menu" class="span1">
+
+		</div>
+
+	</div>
+</div>
