f7162bbdc9c443f8b8f303dc0d49db5f26928a69
mmmartin <martinez.mz10@gmail.com>
2012-09-05 18:49:09 -0500

    Agregue Modals a http://localhost/secundarias/index.php/pruebas/noticias y en http://localhost/secundarias/index.php/publicacion/comentario

--- application/views/pruebas/noticias.php
+++ application/views/pruebas/noticias.php
@@ -174,7 +174,7 @@
 	</div>
 </div>
 
-<-- Modal -->
+<!-- Modal -->
 <div class="modal hide" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
   <div class="modal-header">
     <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
--- application/views/publicacion/comentarios.php
+++ application/views/publicacion/comentarios.php
@@ -126,7 +126,7 @@
 
 
 
-<-- Modal -->
+<!-- Modal -->
 <div class="modal hide" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
   <div class="modal-header">
     <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
