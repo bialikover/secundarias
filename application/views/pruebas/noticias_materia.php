4888308577d78b5fcf76ead102f45376b54d0477
Adánowsky Galván González <bialikoer@gmail.com>
2012-09-06 15:44:17 -0500

    add neto uncommited

--- application/views/pruebas/noticias_materia.php
+++ application/views/pruebas/noticias_materia.php
@@ -1,136 +1,61 @@
-<h1>Matemáticas I</h1>
+<h1>Materias</h1>
 <hr>
-<div class="row-floud">
-	<div class="span6">
-		<div class="row-fluid"> 
-	
-			<div class="span12">
-
-				<div class="my-comentary-container">
-				<div class="my-comentary-header">Actividades Recientes </div>
-				<div class="my-comentary">
-				<div class="row-fluid">
-					<div class="span2 my-center">
-						<img class="my-foto-mini" src="<?php echo base_url();?>/assets/img/user1.png">
-					</div>
-					<div class="span10">
-						<h4>Dharwin Borracho</h4>
-						<p>Esta es una publicación del Dharwin. 
-							Esta es una publicación del Dharwin. 
-							Esta es una publicación del Dharwin. 
-							Esta es una publicación del Dharwin. </p>
-
-							<div class="date-footer">
-							<span>05/09/2012 12:00:00</span>
-						</div>
-					</div>
-				</div>
-				</div>
-				<div class="my-comentary">
-				<div class="row-fluid">
-					<div class="span2 my-center">
-						<img class="my-foto-mini" src="<?php echo base_url();?>/assets/img/user1.png">
-					</div>
-					<div class="span10">
-						<h4>Dharwin Borracho</h4>
-						<p>Esta es una publicación del Dharwin. 
-							Esta es una publicación del Dharwin. 
-							Esta es una publicación del Dharwin. 
-							Esta es una publicación del Dharwin. </p>
-
-							<div class="date-footer">
-							<span>05/09/2012 12:00:00</span>
-						</div>
-					</div>
-				</div>
-				</div>
-				<div class="my-comentary">
-				<div class="row-fluid">
-					<div class="span2 my-center">
-						<img class="my-foto-mini" src="<?php echo base_url();?>/assets/img/user1.png">
-					</div>
-					<div class="span10">
-						<h4>Dharwin Borracho</h4>
-						<p>Esta es una publicación del Dharwin. 
-							Esta es una publicación del Dharwin. 
-							Esta es una publicación del Dharwin. 
-							Esta es una publicación del Dharwin. </p>
-
-							<div class="date-footer">
-							<span>05/09/2012 12:00:00</span>
-						</div>
-					</div>
-				</div>
-				</div>
-			</div>
-			</div>
-		</div>
-	</div>
-	
-	<div class="span6">
-		<div class="row-fluid"> 
-			
-			<div id="my-menu" class="span12">
-
-				<div class="my-comentary-container">
-				<div class="my-comentary-header">Comentarios Recientes</div>
-				<div class="my-comentary">
-				<div class="row-fluid">
-					<div class="span2 my-center">
-						<img class="my-foto-mini" src="<?php echo base_url();?>/assets/img/user1.png">
-					</div>
-					<div class="span10">
-						<h4>Dharwin Borracho</h4>
-						<p>Esta es una publicación del Dharwin. 
-							Esta es una publicación del Dharwin. 
-							Esta es una publicación del Dharwin. 
-							Esta es una publicación del Dharwin. </p>
-
-							<div class="date-footer">
-							<span>05/09/2012 12:00:00</span>
-						</div>
-					</div>
-				</div>
-				</div>
-				<div class="my-comentary">
-				<div class="row-fluid">
-					<div class="span2 my-center">
-						<img class="my-foto-mini" src="<?php echo base_url();?>/assets/img/user1.png">
-					</div>
-					<div class="span10">
-						<h4>Dharwin Borracho</h4>
-						<p>Esta es una publicación del Dharwin. 
-							Esta es una publicación del Dharwin. 
-							Esta es una publicación del Dharwin. 
-							Esta es una publicación del Dharwin. </p>
-
-							<div class="date-footer">
-							<span>05/09/2012 12:00:00</span>
-						</div>
-					</div>
-				</div>
-				</div>
-				<div class="my-comentary">
-				<div class="row-fluid">
-					<div class="span2 my-center">
-						<img class="my-foto-mini" src="<?php echo base_url();?>/assets/img/user1.png">
-					</div>
-					<div class="span10">
-						<h4>Dharwin Borracho</h4>
-						<p>Esta es una publicación del Dharwin. 
-							Esta es una publicación del Dharwin. 
-							Esta es una publicación del Dharwin. 
-							Esta es una publicación del Dharwin. </p>
-
-							<div class="date-footer">
-							<span>05/09/2012 12:00:00</span>
-						</div>
-					</div>
-				</div>
-				</div>
-			</div>
-			</div>
-
-		</div>
-	</div>
+<div class="row-fluid"> 
+    <div class="12"> 
 </div>
+<h1>Lo Nuevo</h1>
+<?php foreach ($contenidos as $contenido) { ?>
+<div class="row-floud">
+        <div class="span6">
+            <div class="row-fluid"> 
+                <div class="span12">
+                    <div class="my-comentary-container">
+                        <div class="my-comentary-header">Tema</div>
+                        <div class="my-comentary">
+                            <div class="row-fluid">
+                                <div class="span2 my-center">
+                                    <img class="my-foto-mini" src="<?php echo base_url(); ?>/assets/img/user1.png">
+                                </div>
+                                <div class="span10">
+                                    <h4><?php echo $docente->nombre; ?></h4>
+                                    <p><?php echo $contenido['contenido']->texto; ?></p>
+
+                                    <div class="date-footer">
+                                        <span><?php $contenido['contenido']->fecha_publicacion; ?></span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="span6">
+            <div class="row-fluid"> 
+                <div class="span12">
+                    <div class="my-comentary-container">
+                        <div class="my-comentary-header">Comentarios</div>
+                        <?php foreach ($contenido['comentarios'] as $comentario) { ?>
+                            <div class="my-comentary">
+
+                                <div class="row-fluid">
+                                    <div class="span2 my-center">
+                                        <img class="my-foto-mini" src="<?php echo base_url(); ?>/assets/img/user1.png">
+                                    </div>
+                                    <div class="span10">
+                                        <h4><?php $comentario['autor']->nombre; ?></h4>
+                                        <p><?php echo $comentario['comentario']->contenido; ?></p>
+
+                                        <div class="date-footer">
+                                            <span><?php echo $comentario['comentario']->fecha_publicacion; ?></span>
+                                        </div>
+                                    </div>
+                                </div></div>
+                        <?php } ?>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+</div>
+<?php } ?>
