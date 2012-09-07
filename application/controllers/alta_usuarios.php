1fe5561b3e6d6bc40dced1b421303414f0784744
Adánowsky Galván González <bialikoer@gmail.com>
2012-09-07 13:33:37 -0500

    add users crud

--- application/controllers/alta_usuarios.php
+++ application/controllers/alta_usuarios.php
@@ -8,8 +8,7 @@ class Alta_usuarios extends CI_Controller
 		parent::__construct();
 		
 		$this->load->database();
-		$this->load->helper('url');
-		
+		$this->load->helper('url');		
 		$this->load->library('grocery_CRUD');
 	}
 
@@ -20,19 +19,56 @@ class Alta_usuarios extends CI_Controller
 		$crud->set_theme('datatables');
 		$crud->set_subject('usuario');
     	$crud->set_table('usuario');
+		$crud->set_relation('tipoUsuarioId','tipo_usuario','tipoUsuario');
+    	$crud->columns('usuarioId', 'usuario','password', 'tipoUsuarioId');
+    	$crud->fields('usuario','password', 'verify_password', 'tipoUsuarioId');
+    	$crud->display_as('tipoUsuarioId','Tipo de usuario');
 
-    	$crud->display_as('user','Usuario');
+        $crud->change_field_type('password', 'password');
+        $crud->change_field_type('verify_password', 'password');
+        $crud->set_rules('verify_password', 'Verify Password', 'required|matches[password]');
+ 		
 
-    	$crud->columns('id_usuario', 'matricula','role');
-
+ 		//callbacks
+    	$crud->callback_before_insert(array($this,'encrypt_password_callback'));
+    	$crud->callback_before_update(array($this,'encrypt_password_callback'));
+    	$crud->callback_edit_field('password',array($this,'decrypt_password_callback'));
+    	$crud->callback_before_insert(array($this,'unset_verification'));
+		$crud->callback_before_update(array($this,'unset_verification'));
+    	
     	$output = $crud->render();
     	
     	$this->load->view('includes/header');
-    	$this->load->view('alumnos/index',$output);
+    	$this->load->view('alta_usuarios/index',$output);
     	$this->load->view('includes/footer');
-
-
 	}
+
+	function encrypt_password_callback($post_array, $primary_key = null){
+    	$this->load->library('encrypt');
+ 
+    	$key = 'k1PAjW3tuHCjewV7p7gFEiHps501b68d';
+    	$post_array['password'] = $this->encrypt->encode($post_array['password'], $key);
+    	return $post_array;
+	}
+ 
+	function decrypt_password_callback($value)
+	{
+	    $this->load->library('encrypt');
+
+    	$key = 'k1PAjW3tuHCjewV7p7gFEiHps501b68d';
+    	$decrypted_password = $this->encrypt->decode($value, $key);
+    	return "<input type='password' name='password' value='$decrypted_password' />";
+	}
+
+	function unset_verification($post_array) {
+           unset($post_array['verify_password']);
+           return $post_array;
+	}
+
+
+
+
+
 }
 
 
--- application/controllers/pruebas.php
+++ application/controllers/pruebas.php
@@ -7,18 +7,7 @@ class Pruebas extends CI_Controller
 		$this->load->helper('url');
 	}
 
-<<<<<<< HEAD
-	public function noticias()
-	{
-    	$this->load->view('includes/header-escuela');
-    	$this->load->view('pruebas/noticias');
-    	$this->load->view('includes/footer');
-	}
-	public function noticias_materia()
-	{
-    	$this->load->view('includes/header-escuela');
-    	$this->load->view('pruebas/noticias_materia');
-=======
+
 	public function noticias($data=NULL)
 	{
     	$this->load->view('includes/header-escuela');
@@ -28,8 +17,7 @@ class Pruebas extends CI_Controller
 	public function noticias_materia($data=NULL)
 	{
     	$this->load->view('includes/header-escuela');
-    	$this->load->view('pruebas/noticias_materia',$data);
->>>>>>> adanowsky
+    	$this->load->view('pruebas/noticias_materia',$data); 
     	$this->load->view('includes/footer');
 	}
 }
