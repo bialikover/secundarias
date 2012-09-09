a07b2ba3aa9170d49716b2faadea8c115e328e75
localadmin <localadmi@server.telmex.com>
2012-09-08 20:20:00 -0500

    Pural a singular

--- application/controllers/alta_usuarios.php
+++ application/controllers/alta_usuarios.php
@@ -3,77 +3,67 @@
 
 class Alta_usuarios extends CI_Controller
 {
-	function __construct()
-	{
-		parent::__construct();
-		
-		$this->load->database();
-		$this->load->helper('url');		
-		$this->load->library('grocery_CRUD');
-	}
+    function __construct()
+    {
+        parent::__construct();
+        
+        $this->load->database();
+        $this->load->helper('url');     
+        $this->load->library('grocery_CRUD');
+    }
 
-	public function index()
-	{
+    public function index()
+    {
 
-		$crud = new grocery_CRUD();
-		$crud->set_theme('datatables');
-		$crud->set_subject('usuario');
-    	$crud->set_table('usuario');
-		$crud->set_relation('tipoUsuarioId','tipo_usuario','tipoUsuario');
-    	$crud->columns('usuarioId', 'usuario','password', 'tipoUsuarioId');
-
-    	$crud->fields('usuario','password', 'tipoUsuarioId');
-    	$crud->display_as('tipoUsuarioId','Tipo de usuario');
+        $crud = new grocery_CRUD();
+        $crud->set_theme('datatables');
+        $crud->set_subject('usuario');
+        $crud->set_table('usuario');
+        $crud->set_relation('tipoUsuarioId','tipo_usuario','tipoUsuario');
+        $crud->columns('usuarioId', 'usuario','password', 'tipoUsuarioId');
+        $crud->fields('usuario','password', 'verificar_password', 'tipoUsuarioId');
+        $crud->display_as('tipoUsuarioId','Tipo de usuario');
 
         $crud->change_field_type('password', 'password');
-<<<<<<< HEAD
-        //$crud->change_field_type('verificar_password', 'password');
-        //$crud->set_rules('verificar_password', 'Verificar Password', 'required|matches[password]');
-=======
         $crud->change_field_type('verificar_password', 'password');
         $crud->set_rules('verificar_password', 'Verificar Password', 'required|matches[password]');
- 		
->>>>>>> 3d80175f16e1775c971cb0fc96eb22f4277f5105
-
-        $crud->add_action('domicilio', '', 'domicilio/index/edit', 'icon-image');
- 		
         
- 		//callbacks
-    	$crud->callback_before_insert(array($this,'encrypt_password_callback'));
-    	$crud->callback_before_update(array($this,'encrypt_password_callback'));
-    	$crud->callback_edit_field('password',array($this,'decrypt_password_callback'));
-    	$crud->callback_before_insert(array($this,'unset_verification'));
-		$crud->callback_before_update(array($this,'unset_verification'));
-    	
-    	$output = $crud->render();
-    	
-    	$this->load->view('includes/header');
-    	$this->load->view('alta_usuarios/index',$output);
-    	$this->load->view('includes/footer');
-	}
 
-	function encrypt_password_callback($post_array, $primary_key = null){
-    	$this->load->library('encrypt');
+        //callbacks
+        $crud->callback_before_insert(array($this,'encrypt_password_callback'));
+        $crud->callback_before_update(array($this,'encrypt_password_callback'));
+        $crud->callback_edit_field('password',array($this,'decrypt_password_callback'));
+        $crud->callback_before_insert(array($this,'unset_verification'));
+        $crud->callback_before_update(array($this,'unset_verification'));
+        
+        $output = $crud->render();
+        
+        $this->load->view('includes/header');
+        $this->load->view('alta_usuarios/index',$output);
+        $this->load->view('includes/footer');
+    }
+
+    function encrypt_password_callback($post_array, $primary_key = null){
+        $this->load->library('encrypt');
  
-    	$key = 'k1PAjW3tuHCjewV7p7gFEiHps501b68d';
-    	$post_array['password'] = $this->encrypt->encode($post_array['password'], $key);
-    	return $post_array;
-	}
+        $key = 'k1PAjW3tuHCjewV7p7gFEiHps501b68d';
+        $post_array['password'] = $this->encrypt->encode($post_array['password'], $key);
+        return $post_array;
+    }
  
-	function decrypt_password_callback($value)
-	{
-	    $this->load->library('encrypt');
+    function decrypt_password_callback($value)
+    {
+        $this->load->library('encrypt');
 
-    	$key = 'k1PAjW3tuHCjewV7p7gFEiHps501b68d';
-    	$decrypted_password = $this->encrypt->decode($value, $key);
-    	return "<input type='password' name='password' value='$decrypted_password' />";
-	}
+        $key = 'k1PAjW3tuHCjewV7p7gFEiHps501b68d';
+        $decrypted_password = $this->encrypt->decode($value, $key);
+        return "<input type='password' name='password' value='$decrypted_password' />";
+    }
 
-	function unset_verification($post_array) {
+    function unset_verification($post_array) {
            unset($post_array['verificar_password']);
            return $post_array;
-	}
+    }
 }
-
 
 ?>
---
+++ application/controllers/datos_personal.php
@@ -0,0 +1,47 @@
+<?php
+
+class Datos_personal extends CI_Controller
+{
+
+	function __construct()
+	{
+		parent::__construct();
+		
+		$this->load->database();
+		$this->load->helper('url');
+		
+		$this->load->library('grocery_CRUD');
+	}
+
+	public function index()
+	{
+		$crud = new grocery_CRUD();
+
+            $crud->set_theme('datatables');
+            $crud->set_subject('datos personales');
+            $crud->set_table('datos_personal');
+
+
+            $crud->columns('nombre','aPaterno','aMaterno');
+            $crud->fields('nombre','aPaterno','aMaterno','genero','fechaNac');         
+
+            $crud->display_as('aPaterno','Apellido Paterno');
+            $crud->display_as('aMaterno','Apellido Materno');
+            $crud->display_as('fechaNac','Fecha de Nacimiento');
+
+            $crud->unset_delete();
+            $crud->unset_add_fields();
+            $crud->unset_add();
+            $crud->unset_export();
+            $crud->unset_print();
+
+            $output = $crud->render();
+
+            $this->load->view('includes/header-docente');
+            $this->load->view('datos_personales/index', $output);
+            $this->load->view('includes/footer');
+	}
+
+}
+
+?>
