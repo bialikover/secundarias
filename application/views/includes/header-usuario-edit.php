d50eb54e76dabb360212170eac24515c8937c17b
localadmin <localadmi@server.telmex.com>
2012-09-18 03:27:23 -0500

    Starting with headers

--- application/views/includes/header-usuario-edit.php
+++ application/views/includes/header-usuario-edit.php
@@ -21,11 +21,9 @@
 	<header>
 			<div class="navbar my-nav">
 	  			<div class="navbar-inner">
-					
-						<div class="container-fluid">
+					<div class="container-fluid">
 						<div class="row-fluid"> 
-							
-								<div id="my-menu" class="span12">
+							<div id="my-menu" class="span12">
 									<ul class="nav">
 										<li><a class="" href="<?php echo base_url("index.php/welcome")?>">
 										  	<img id="tab1" class="img-circle my-img-tab" src="<?php echo base_url();?>/assets/img/home-bco.png">
@@ -34,24 +32,18 @@
 										</li>
 									</ul>
 
-							<ul class="nav pull-right">
+									<ul class="nav pull-right">
+						  				<li><a class="my-a"  href="<?php echo base_url("index.php/alta_usuario")?>">  
+						  					<img id="tab2" class="img-circle  my-img-tab" src="<?php echo base_url();?>/assets/img/regresar-bco.png">
+						  					<div id="tab2_t" class="my-tex-nav">Regresar</div>
+						  					</a>
+						  				</li>
+									</ul>
 
-						  <li><a class="my-a"  href="<?php echo base_url("index.php/alta_usuario")?>">  
-						  	<img id="tab2" class="img-circle  my-img-tab" src="<?php echo base_url();?>/assets/img/regresar-bco.png">
-						  	<div id="tab2_t" class="my-tex-nav">Regresar</div>
-						  	</a>
-						  </li>
-
-
-						 
-					</ul>
-
-				</div>
-				<a  href="<?php echo base_url("index.php/welcome/do_logout")?>"> Salir</a>	
-			</div>
-		</div>
-
-
+							</div>
+							<a  href="<?php echo base_url("index.php/welcome/do_logout")?>"> Salir</a>	
+						</div>
+					</div>
 				</div>
 			</div>
 	</header>
