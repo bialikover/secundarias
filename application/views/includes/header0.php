2f609f22646ba39c837f873c4aca2e33a5bca923
mmmartin <martinez.mz10@gmail.com>
2012-09-27 01:46:11 -0500

    jaja

---
+++ application/views/includes/header0.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+		<meta charset="utf-8">
+    	<title>Mi secu</title>
+
+		<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>/assets/css/home-secundarias.css">
+		<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>/assets/css/bootstrap.css">
+		<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>/assets/css/bootstrap-responsive.css">
+		
+		<script type="text/javascript" src="<?php echo base_url();?>/assets/js/jquery.js"></script> 
+		<script type="text/javascript" src="<?php echo base_url();?>/assets/js/bootstrap.js"></script> 
+		
+	</head>
+	<body>
+	<header>
+	</header>
