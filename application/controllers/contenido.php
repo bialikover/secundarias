1958791f8c20c0bd5ede1e9181a87c8a8ae6bcf8
Adánowsky Galván González <bialikoer@gmail.com>
2012-09-29 07:01:37 -0500

    grupo docente is not working in actividad

--- application/controllers/contenido.php
+++ application/controllers/contenido.php
@@ -23,16 +23,20 @@ class Contenido extends CI_Controller
 
 
   public function nuevo(){
-   if (!$this->session->userdata('validated') ||  $this->session->userdata('tipoUsuarioId') != 3 ) {
+   if (!$this->session->userdata('validated') ||  $this->session->userdata('tipoUsuarioId') > 3 ) {
         redirect('login');
     } else {  
       $crud = new grocery_CRUD();
       $crud->set_table('actividad');
       $crud->set_theme('datatables');
+
       $crud->columns('nombreActividad','descActividad');
       $crud->fields('nombreActividad','descActividad', 'tipoActividadId', 'fecha', 'rutaActividad', 'grupo_docente_materiaId');
-      $crud->set_relation('grupo_docente_materiaId', 'grupo_docente_materia', '{claveGrupo} - {nombreMateria1}');
+      $crud->set_relation('grupo_docente_materiaId', 'grupo_docente_materia', '{claveGrupo} - {nombreMateria}');
       $crud->display_as('grupo_docente_materiaId','Grupo Materia');
+      
+      $crud->callback_add_field('grupo_docente_materiaId',array($this,'materias'));
+      
       $crud->change_field_type('tipoActividadId','invisible');
       $crud->change_field_type('fecha','invisible');
       $crud->set_field_upload('rutaActividad','index.php/assets/uploads/files');
@@ -51,33 +55,23 @@ class Contenido extends CI_Controller
 
   function tipo_actividad_fecha($post_array){
   	$post_array['tipoActividadId'] = 2;
-  	$post_array['fecha'] = date("Y-m-d h:m:s");
+  	$post_array['fecha'] = date("Y-m-d H:i:s");
   	return $post_array;
   }
   
     public function materias(){
-    $usuarioId = $this->session->userdata("usuarioId"); 
-            $id_docentes = $this->db->query("select id_docentes from docentes where id_docentes =". $perfil_id);
-            $id_docentes = $id_docentes->row()->id_docentes;
+      $usuarioId = $this->session->userdata("usuarioId"); 
+      $this->load->model("materia_model");
+      $materias = $this->materia_model->clave_materias_docente($usuarioId);
+      //var_dump($materias);
+      //die();
+      $html1 ="<select id='field-id_materias'  name='id_materias' class='chosen-select' data-placeholder='Select Materia' style='width:300px'>";
+      foreach ( $materias as $materia ) {
+        $html1 .= "<option value='".$materia->grupo_docente_materiaId."'>".$materia->claveGrupo." - ".$materia->nombreMateria1."</option>";
+      }
 
-            $materias = $this->db->query(
-            "select nombre,id_materia from `materias` where `id_materia` IN (
-                 select `id_materia` from `docente_materias` where `id_docente` =". $id_docentes. "
-             );"
-            );
-          
-          $materia = $materias->result();
-          
-          $html1 ="<select id='field-id_materias'  name='id_materias' class='chosen-select' data-placeholder='Select Materia' style='width:300px'>";
-          foreach ( $materia as $mat ) {
-            $html1 .= "<option value='".$mat->id_materia."'>".$mat->nombre."</option>";
-          }
-
-          $html1 .= '</select>';
-
-          /////////
-
-          return $html1;
+      $html1 .= '</select>';
+      return $html1;
 
   }
 
