7e5a8daf5f6f73d4e9ea05bf83ddb04c4cf1b376
Jesus Lopez <charrosnegrosborrachos@hotmail.com>
2012-09-30 23:35:14 -0500

    Database modified 1

--- secundaria_db.sql
+++ secundaria_db.sql
@@ -526,7 +526,7 @@ CREATE TABLE IF NOT EXISTS `actividad` (
 
 DROP TABLE IF EXISTS `comentario`;
 CREATE TABLE IF NOT EXISTS `comentario` (
-  `comentarioId` bigint(20) NOT NULL,
+  `comentarioId` bigint(20) NOT NULL AUTO_INCREMENTh,
   `comentario` varchar(100) COLLATE utf8_bin DEFAULT NULL,
   `usuarioId` bigint(20) NOT NULL,
   `fecha` datetime DEFAULT NULL,
