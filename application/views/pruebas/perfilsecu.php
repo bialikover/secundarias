5fe335f13c113aae818a98d5cacc24fb0039b340
mmmartin <martinez.mz10@gmail.com>
2012-10-09 16:35:08 -0500

    add perfil secu

---
+++ application/views/pruebas/perfilsecu.php
@@ -0,0 +1,119 @@
+<div class="container-fluid">
+
+	<h2>Perfil de Secundaria</h2>
+	<hr>
+
+  <div class="row-fluid">
+    <div class="span1">
+      <!--Sidebar content-->
+    </div>
+    <div class="span4">
+      <img class="my-img-secu" src="<?php echo base_url();?>assets/img/secu1.png">
+    </div>
+    <div class="span6">
+      <h3>Secundaria 66 Vicente Guerrero</h3>
+      <p>
+      	Secundaria 66 Vicente Guerrero, 
+      	Secundaria 66 Vicente Guerrero, 
+      	Secundaria 66 Vicente Guerrero, 
+      	Secundaria 66 Vicente Guerrero,
+      	Secundaria 66 Vicente Guerrero, 
+      	Secundaria 66 Vicente Guerrero, 
+      	Secundaria 66 Vicente Guerrero, 
+      </p>
+    </div>
+    <div class="span1">
+      <!--Body content-->
+    </div>
+  </div>
+
+
+<hr>
+  <div class="row-fluid">
+    <div class="span1">
+      <!--Sidebar content-->
+    </div>
+    <div class="span4">
+      <h3>Datos</h3>
+
+      <h4>Turno</h4>
+      <p>texto, texto</p>
+      <h4>Clave</h4>
+      <p>texto, texto</p>
+      <h4>Zona</h4>
+      <p>texto, texto</p>
+      <h4>Sector Educativo</h4>
+      <p>texto, texto</p>
+      <h4>Localidad</h4>
+      <p>texto, texto</p>
+      <h4>Municipio</h4>
+      <p>texto, texto</p>
+
+    </div>
+    <div class="span6">
+    	<h3>Ubicación</h3>
+      <iframe width="600" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://maps.google.es/maps?f=q&amp;source=s_q&amp;hl=es&amp;geocode=&amp;q=DF,+M%C3%A9xico&amp;aq=0&amp;oq=mexico+df&amp;sll=37.230328,2.856445&amp;sspn=19.861539,39.331055&amp;ie=UTF8&amp;hq=&amp;hnear=Ciudad+de+M%C3%A9xico,+Distrito+Federal,+M%C3%A9xico&amp;t=m&amp;ll=19.4326,-99.133072&amp;spn=0.129506,0.206337&amp;z=12&amp;output=embed"></iframe><br />
+    </div>
+    <div class="span1">
+      <!--Body content-->
+    </div>
+  </div>
+
+<hr>
+<div class="row-fluid">
+    <div class="span1">
+    </div>
+    <div class="span10">
+    <div id="my-carousel" class="my-carousel">
+      <div id="myCarousel" class="carousel slide ">
+        <!-- Carousel items -->
+        <div class="carousel-inner">
+
+          
+          <div class="active item">
+            <img src="<?=base_url('assets/img/img1.jpg')?>">
+            <div class="carousel-caption">
+              <h4>Información</h4>
+                <p>Más información, Más información, Más información, Más información...</p>
+            </div>
+          </div>
+
+          <div class="item">
+            <img src="<?=base_url('assets/img/img2.jpg')?>">
+            <div class="carousel-caption">
+              <h4>Información</h4>
+                <p>Más información, Más información, Más información, Más información...</p>
+            </div>
+          </div>
+
+          <div class="item">
+          <img src="<?=base_url('assets/img/img3.jpg')?>">
+          <div class="carousel-caption">
+              <h4>Información</h4>
+                <p>Más información, Más información, Más información, Más información...</p>
+            </div>
+          </div>
+
+          <div class="item">
+          <img src="<?=base_url('assets/img/img4.jpg')?>">
+          <div class="carousel-caption">
+              <h4>Información</h4>
+                <p>Más información, Más información, Más información, Más información...</p>
+            </div>
+          </div>
+
+      
+      </div>
+
+        <!-- Carousel nav -->
+        <a class="carousel-control left" href="#myCarousel" data-slide="prev">&lsaquo;</a>
+        <a class="carousel-control right" href="#myCarousel" data-slide="next">&rsaquo;</a>
+      </div>
+      
+          </div>
+    </div>
+    <div class="span1">
+    </div>
+      </div>
+
+</div>
