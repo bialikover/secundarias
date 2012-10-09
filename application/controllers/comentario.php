dc51b5df40d3f6ce57fbac3899fdd1ba85ba15bb
Adánowsky Galván González <bialikoer@gmail.com>
2012-10-08 20:22:54 -0500

    add post validation

--- application/controllers/comentario.php
+++ application/controllers/comentario.php
@@ -10,21 +10,24 @@ class Comentario extends CI_Controller {
     }
 
     function nuevo(){
-        $this->load->model('comentario_model');
-        $result = $this->comentario_model->guardar();
-        if($this->session->userdata("validated") && $result['status'] == true){
-        	if($this->input->post('stream') != false){
-        		$html = $this->load->view("comentario/comentario", $result['data'], true);
-        		echo $html;
-        	} else{
+        $this->load->model('comentario_model');        
+        if($this->session->userdata("validated")){
+            $result = $this->comentario_model->guardar();
+            if( $result['status'] == true){
+        	   if($this->input->post('stream') != false){
+        		  $html = $this->load->view("comentario/comentario", $result['data'], true);
+        		  echo $html;
+        	   } else{
 
-        		$html = $this->load->view("comentario/comentario_stream", $result['data'], true);
-        		echo $html;
-        	}
-
-      	} else{
-        	return false;
-      	}
+        		  $html = $this->load->view("comentario/comentario_stream", $result['data'], true);
+        		  echo $html;
+        	   }
+            } else{
+        	   return false;
+      	    }
+        } else{
+            return false;
+        }
         
     }
 
