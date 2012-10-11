b0fea44611e8bb92105288d53cbea8e89977e5bb
mmmartin <martinez.mz10@gmail.com>
2012-10-11 09:27:05 -0500

    Update perfilSecu

--- application/views/escuela/perfilsecu.php
+++ application/views/escuela/perfilsecu.php
@@ -1,66 +1,16 @@
 <?php //var_dump($escuela);?>
-<?php var_dump($noticias);?>
+<!-- <?php var_dump($noticias);?> -->
+
+
 <div class="container-fluid">
-
-	<h2>Perfil de Secundaria</h2>
+	
+  <h2>Perfil de Secundaria</h2>
 	<hr>
-<div class="row-fluid">
-    <div class="span1">
-    </div>
-    <div class="span10">
-    <div id="my-carousel" class="my-carousel">
-      <div id="myCarousel" class="carousel slide ">
-        <!-- Carousel items -->
-        <div class="carousel-inner">
-          <?php foreach ($noticias as $noticia):?>
-          
-          <div class="item">
-            <img src="<?php echo base_url("assets/uploads/fotosEscuela/".$noticia->rutaActividad);?>">
-            <div class="carousel-caption">
-              <h4><?php echo $noticia->nombreActividad;?></h4>
-                <p><?php echo $noticia->descActividad;?></p>
-            </div>
-          </div>
-
-      <?php endforeach;?>
-      </div>
-
-        <!-- Carousel nav -->
-        <a class="carousel-control left" href="#myCarousel" data-slide="prev">&lsaquo;</a>
-        <a class="carousel-control right" href="#myCarousel" data-slide="next">&rsaquo;</a>
-      </div>
-      
-          </div>
-    </div>
-    <div class="span1">
-    </div>
-      </div>
-      <hr>
   <div class="row-fluid">
-    <div class="span1">
-      <!--Sidebar content-->
-    </div>
-    <div class="span4">
+    
+    <!-- Columna Estatica  - - - - - - - - - - - - - - - - - -->
+    <div class="span2">
       <img class="my-img-secu" src="<?php echo base_url("assets/uploads/fotosEscuela/".$escuela->rutaFotoEscuela);?>">
-    </div>
-    <div class="span6">
-      <h3><?php echo $escuela->claveEscuela." - ".$escuela->escuela?></h3>
-      <p>
-      	<?php echo $escuela->adicional?>
-      </p>
-    </div>
-    <div class="span1">
-      <!--Body content-->
-    </div>
-  </div>
-
-
-<hr>
-  <div class="row-fluid">
-    <div class="span1">
-      <!--Sidebar content-->
-    </div>
-    <div class="span4">
       <h3>Datos</h3>
 
       <h4>Direccion</h4>
@@ -74,19 +24,57 @@
       <h4>Localidad</h4>
       <p><?php echo $escuela->localidad;?></p>
       <h4>Municipio</h4>
-      <p><?php echo $escuela->municipio;?></p>
+     <!-- <p><?php echo $escuela->municipio;?></p>-->
+
+      <a href=""><span class="btn btn-success">Docentes</span></a>
 
     </div>
-    <div class="span6">
-    	<h3>Ubicación</h3>
-      <iframe width="600" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://maps.google.es/maps?f=q&amp;source=s_q&amp;hl=es&amp;geocode=&amp;q=DF,+M%C3%A9xico&amp;aq=0&amp;oq=mexico+df&amp;sll=37.230328,2.856445&amp;sspn=19.861539,39.331055&amp;ie=UTF8&amp;hq=&amp;hnear=Ciudad+de+M%C3%A9xico,+Distrito+Federal,+M%C3%A9xico&amp;t=m&amp;ll=<?php echo $escuela->latitud;?>,<?php echo $escuela->longitud;?>&amp;spn=0.129506,0.206337&amp;z=12&amp;output=embed"></iframe><br />
+<div class="span1">
+</div>
+    <div class="span9">
+
+      <!-- Carrusel - - - - - - - - - - - - - - - - - - - - -->
+      <div id="my-carousel" class="my-carousel">
+        <div id="myCarousel" class="carousel slide ">
+          <!-- Carousel items -->
+          <div class="carousel-inner">
+            <?php foreach ($noticias as $noticia):?>
+            
+            <div class="item">
+              <img src="<?php echo base_url("assets/uploads/fotosEscuela/".$noticia->rutaActividad);?>">
+              <div class="carousel-caption">
+                <h4><?php echo $noticia->nombreActividad;?></h4>
+                  <p><?php echo $noticia->descActividad;?></p>
+              </div>
+            </div>
+
+        <?php endforeach;?>
+        </div>
+
+          <!-- Carousel nav -->
+          <a class="carousel-control left" href="#myCarousel" data-slide="prev">&lsaquo;</a>
+          <a class="carousel-control right" href="#myCarousel" data-slide="next">&rsaquo;</a>
+        </div>
+      </div>
+      <!-- Fin Carrusel - - - - - - - - - - - - - - - - - - - - -->
+
+
+      <!-- Nombre y Descripcion de escuela -->
+      <h3>
+        <?php echo $escuela->claveEscuela." - ".$escuela->escuela?>
+      </h3>
+      <p>
+        <?php echo $escuela->adicional.$escuela->descEscuela?>
+      </p>
+
+      <hr>
+      <!-- Mapa  - - - - - - - - - - - - - - - - - - - - - - - -->
+      <h3>Ubicación</h3>
+      <iframe width="800" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://maps.google.es/maps?f=q&amp;source=s_q&amp;hl=es&amp;geocode=&amp;q=DF,+M%C3%A9xico&amp;aq=0&amp;oq=mexico+df&amp;sll=37.230328,2.856445&amp;sspn=19.861539,39.331055&amp;ie=UTF8&amp;hq=&amp;hnear=Ciudad+de+M%C3%A9xico,+Distrito+Federal,+M%C3%A9xico&amp;t=m&amp;ll=<?php echo $escuela->latitud;?>,<?php echo $escuela->longitud;?>&amp;spn=0.129506,0.206337&amp;z=12&amp;output=embed"></iframe>
+
     </div>
-    <div class="span1">      
-      <!--Body content-->
-    </div>
+
+
+
   </div>
-
-<hr>
-<a href="<?php echo base_url("/docente");?>"><h3>Ver cuerpo docente</h3></a>
-
 </div>
