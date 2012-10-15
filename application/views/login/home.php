55174b6cb46f4796eff9c09629f5fce8d9592b49
Adánowsky Bialikover <bialikoer@gmail.com>
2012-10-15 16:39:19 -0500

    change db long and home img desc

--- application/views/login/home.php
+++ application/views/login/home.php
@@ -65,8 +65,8 @@
 			    <div class="item">
 			    	<img src="<?=base_url().'assets/img/img2.jpg'?>">
 			    	<div class="carousel-caption">
-			    		<h4>Información</h4>
-			    			<p>Más información, Más información, Más información, Más información...</p>
+			    		<h4>Mis Materias</h4>
+			    			<p>Navega a través de tus materias para tus actividades extraclase.</p>
 			    	</div>
 			    </div>
 
@@ -82,7 +82,7 @@
 					<img src="<?=base_url().'assets/img/img4.jpg'?>">
 					<div class="carousel-caption">
 			    		<h4>Información</h4>
-			    			<p>Más información, Más información, Más información, Más información...</p>
+			    			<p>Navega a través de tus materias para tus actividades extraclase.</p>
 			    	</div>
 			    </div>
 
